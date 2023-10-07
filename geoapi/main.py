952d0e6d07195773122252c1e947e93401b21c94
Andrii Malchyk <snooki17@gmail.com>
2023-10-07 23:02:41 +0300

    Base paths added
    Signed-off-by: Andrii Malchyk <snooki17@gmail.com>

---
+++ geoapi/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from typing import Union
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/nearby/")
+async def get_nearby_fields(point: int, distance: int):
+    return {"point": point, "distance": distance}
+
+
+@app.get("/inside/")
+async def get_fields_inside_parallelogram(x1: int, y1: int, x2: int, y2: int, x3: int, y3: int, x4: int, y4: int):
+    return {"message": "inside"}
+
+
+@app.get("/intersect/")
+async def get_intersect_fields(geometry: str):
+    return {"geometry": geometry}
+
+
+@app.get("/stats/")
+async def get_region_stats(region: str):
+    return {"region": region}
