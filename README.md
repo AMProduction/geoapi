1e8fc708b55f9734d38966755ca9335e05da89b7
Andrii Malchyk <snooki17@gmail.com>
2023-10-11 22:55:09 +0300

    Added DOCS
    Signed-off-by: Andrii Malchyk <snooki17@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# GeoAPI v1.0
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![CodeQL](https://github.com/AMProduction/geoapi/actions/workflows/github-code-scanning/codeql/badge.svg?branch=main)](https://github.com/AMProduction/geoapi/actions/workflows/github-code-scanning/codeql) [![Build and Push Docker Image to Docker Hub](https://github.com/AMProduction/geoapi/actions/workflows/docker-hub.yml/badge.svg?branch=main)](https://github.com/AMProduction/geoapi/actions/workflows/docker-hub.yml) ![Docker Image Version (latest semver)](https://img.shields.io/docker/v/andruxa17/geoapi) ![Docker Image Size with architecture (latest by date/latest semver)](https://img.shields.io/docker/image-size/andruxa17/geoapi)
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![FastAPI](https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi) ![Postgresql](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white) ![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white) ![GitHub Actions](https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white)
+
+## The project description
+
+Designed and implemented the API that allows customers to retrieve geometries from the Database by given parameters.
--- geoapi/main.py
+++ geoapi/main.py
@@ -4,7 +4,42 @@ from fastapi import FastAPI, HTTPException
 
 from src import api_functions as af
 
-app = FastAPI()
+tags_metadata = [{"name": "nearby",
+                  "description": "Get the set of fields that are 'nearby' given point within a given distance.", },
+                 {"name": "inside",
+                  "description": "Get the set of fields that are inside the parallelogram with given vertices", },
+                 {"name": "intersect",
+                  "description": "Get the set of fields that intersect with the requested geometry", },
+                 {"name": "stats",
+                  "description": "Get the area, gross yield, and weighted average yield per hectare in a region", }, ]
+
+description = """
+Designed and implemented the API that allows customers to retrieve geometries from the Database by given parameters.
+
+## Nearby
+
+Get the set of fields that are "nearby" given point within a given distance. Point and distance are sent by the user. 
+Radius is meant to be in meters.
+
+## Inside
+
+Get the set of fields that are inside the parallelogram with given vertices. Vertices are sent by the user.
+
+## Intersect
+
+Get the set of fields that intersect with the requested geometry. Geometry is sent by the user.
+
+## Stats
+
+Get the area, gross yield, and weighted average yield per hectare in a region that was sent by the user.
+
+"""
+
+app = FastAPI(title="GeoAPI", description=description,
+              summary="Designed and implemented the API that allows customers to retrieve geometries from the "
+                      "Database by given parameters", version="1.0.0",
+              contact={"name": "Andrii Malchyk", "url": "https://www.linkedin.com/in/andrii-malchyk/",
+                       "email": "snooki17@gmail.com", }, license_info={"name": "Apache 2.0", "identifier": "MIT", }, )
 
 
 @app.get("/")
@@ -12,7 +47,7 @@ async def root():
     return {"message": "Welcome to the GeoApi v1.0"}
 
 
-@app.get("/nearby/")
+@app.get("/nearby/", tags=["nearby"])
 async def get_nearby_fields(x: float, y: float, distance: int, crop: Union[str, None] = None):
     result = af.get_nearby_fields(x, y, distance, crop)
     if result:
@@ -22,7 +57,7 @@ async def get_nearby_fields(x: float, y: float, distance: int, crop: Union[str, 
                             detail="The fields that are 'nearby' given point within a given distance are not found")
 
 
-@app.get("/inside/")
+@app.get("/inside/", tags=["inside"])
 async def get_fields_inside_parallelogram(x0: float, y0: float, x1: float, y1: float, x2: float, y2: float, x3: float,
                                           y3: float, crop: Union[str, None] = None):
     result = af.get_fields_inside_parallelogram(x0, y0, x1, y1, x2, y2, x3, y3, crop)
@@ -33,7 +68,7 @@ async def get_fields_inside_parallelogram(x0: float, y0: float, x1: float, y1: f
                             detail="The fields that are inside the parallelogram with given vertices are not found")
 
 
-@app.get("/intersect/")
+@app.get("/intersect/", tags=["intersect"])
 async def get_intersect_fields(geometry: str, crop: Union[str, None] = None):
     result = af.get_intersect_fields(geometry, crop)
     if result:
@@ -43,7 +78,7 @@ async def get_intersect_fields(geometry: str, crop: Union[str, None] = None):
                             detail="The fields that intersect with the requested geometry are not found")
 
 
-@app.get("/stats/")
+@app.get("/stats/", tags=["stats"])
 async def get_region_stats(region: str):
     result = af.get_region_stats(region)
     if result:
